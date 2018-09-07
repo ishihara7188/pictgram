e893b13ad93abd5c699a76c1f5f2b36c31c9a596
totti <r.funk.7188@gmail.com>
2018-09-07 20:05:44 +0900

    コメント機能実装やり直し前

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,20 @@ class CommentsController < ApplicationController
   end
 
   def new
-
+    @comment = Comment.new
   end
 
   def create
     @comment = Comment.new
-    @comment.user = current_user
 
     if @comment.save
       redirect_to topics_path, success: 'コメントを投稿しました'
     else
       redirect_to topics_path, danger: 'コメントの投稿に失敗しました'
     end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
